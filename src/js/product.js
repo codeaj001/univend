a2aeca352fcf0796e1a34ef1866490c6b41ffb9a
Code AJ <codeaj001@gmail.com>
2025-01-16 22:58:59 +0100

    edit.css and products.css

---
+++ src/js/product.js
@@ -0,0 +1,17 @@
+function searchProducts() {
+    const query = document.getElementById('search-input').value;
+    alert(`Searching for: ${query}`);
+}
+
+function viewCart() {
+    alert('Viewing cart...');
+}
+
+function viewProduct(productName) {
+    alert(`Viewing details for: ${productName}`);
+}
+
+function addToCart(event, productName) {
+    event.stopPropagation();
+    alert(`${productName} added to cart!`);
+}
