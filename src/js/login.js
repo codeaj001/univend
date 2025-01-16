47a7aed1ffd3d4ce0ea14e1de9914d7114631be7
Code AJ <codeaj001@gmail.com>
2025-01-16 22:30:46 +0100

    Created edit_profile.html

--- src/js/login.js
+++ src/js/login.js
@@ -9,15 +9,9 @@ function showLogin() {
 }
 
 function login() {
-    // const email = document.getElementById('email').value;
-    // const password = document.getElementById('password').value;
     window.location.href = "../pages/dashboard.html";
 }
 
 function signup() {
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('signup-email').value;
-    const password = document.getElementById('signup-password').value;
-    const campus = document.getElementById('campus').value;
-    alert(`Signed up as: ${name}, Email: ${email}, Campus: ${campus}`);
+    window.location.href = "../pages/edit_profile.html";
 }
