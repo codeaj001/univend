4bc825511da2f0bdaebc21463b5c2e8c4a2820ff
Code AJ <codeaj001@gmail.com>
2025-01-24 15:45:28 +0100

    Dummy images of Products

--- src/js/product.js
+++ src/js/product.js
@@ -1,17 +1,17 @@
-function searchProducts() {
-    const query = document.getElementById('search-input').value;
-    alert(`Searching for: ${query}`);
-}
+// function searchProducts() {
+//     const query = document.getElementById('search-input').value;
+//     alert(`Searching for: ${query}`);
+// }
 
-function viewCart() {
-    alert('Viewing cart...');
-}
+// function viewCart() {
+//     alert('Viewing cart...');
+// }
 
-function viewProduct(productName) {
-    alert(`Viewing details for: ${productName}`);
-}
+// function viewProduct(productName) {
+//     alert(`Viewing details for: ${productName}`);
+// }
 
-function addToCart(event, productName) {
-    event.stopPropagation();
-    alert(`${productName} added to cart!`);
-}
+// function addToCart(event, productName) {
+//     event.stopPropagation();
+//     alert(`${productName} added to cart!`);
+// }
